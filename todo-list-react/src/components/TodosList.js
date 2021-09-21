97419e9ac8de7042b34ecdf703de1d7ae5af32f5
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-09-21 08:54:52 +0300

    Add local storage for data persistence

--- todo-list-react/src/components/TodosList.js
+++ todo-list-react/src/components/TodosList.js
@@ -6,7 +6,7 @@ import TodoItem from "./TodoItem";
         return (
             <ul>
             {this.props.todos.map(todo => (
-            <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps}  deleteTodoProps={this.props.deleteTodoProps}/>
+            <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps}  deleteTodoProps={this.props.deleteTodoProps}  setUpdate={this.props.setUpdate}/>
             ))}
           </ul>
         )
